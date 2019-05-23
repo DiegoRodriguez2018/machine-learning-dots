c2ccd5dcfa6936c7b166664862a539e382e93047
Diego Rodriguez <diego.f.rodriguezh@gmail.com>
2019-05-23 20:25:20 +1000

    Implemented spawn function

--- renderer.js
+++ renderer.js
@@ -3,7 +3,15 @@
 // All of the Node.js APIs are available in this process.
 const Dot = require('./Dot');
 
-const dot1 = new Dot([400,550]);
+function spawn(pos){
+  const dot = new Dot(pos)
+  setInterval(()=>{
+    dot.move();
+    dot.show();
+  }, 500)
+}
+
+// const dot1 = new Dot([400,550]);
 
 const c = document.getElementById('root');
 const ctx = c.getContext('2d');
@@ -13,6 +21,5 @@ ctx.fillRect(0, 0, 800, 600); //clearing the canvas
 
 setInterval(() => {
   ctx.fillRect(0, 0, 800, 600); //clearing the canvas
-  dot1.move();
-  dot1.show();
+  spawn([400,550]);
 }, 500);
