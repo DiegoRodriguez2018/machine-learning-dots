345a9d41d7a570eb4da4b6a940cc5b971caf102b
Diego Rodriguez <diego.f.rodriguezh@gmail.com>
2019-05-24 12:15:51 +1000

    Added velocity and acceleration properties to Dot, improved the refresh rate in renderer.js and created Brain.js

--- renderer.js
+++ renderer.js
@@ -3,15 +3,15 @@
 // All of the Node.js APIs are available in this process.
 const Dot = require('./Dot');
 
-function spawn(pos){
-  const dot = new Dot(pos)
-  setInterval(()=>{
-    dot.move();
-    dot.show();
-  }, 500)
-}
+// function spawn(pos){
+//   const dot = new Dot(pos)
+//   setInterval(()=>{
+//     dot.move();
+//     dot.show();
+//   }, 500)
+// }
 
-// const dot1 = new Dot([400,550]);
+const dot1 = new Dot([400,550]);
 
 const c = document.getElementById('root');
 const ctx = c.getContext('2d');
@@ -19,7 +19,12 @@ const ctx = c.getContext('2d');
 ctx.fillStyle = 'grey';
 ctx.fillRect(0, 0, 800, 600); //clearing the canvas
 
+// 60 hertz
+const frequency = 60;
+const period = (1 / frequency)*1000; // ms
+
 setInterval(() => {
   ctx.fillRect(0, 0, 800, 600); //clearing the canvas
-  spawn([400,550]);
-}, 500);
+  dot1.show();
+  dot1.move();
+}, period);
